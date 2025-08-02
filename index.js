fe33aca67db29f6fb48f50fef5e72a1c73b047be
Anpsujwal <anpsujwal@gmail.com>
2025-08-02 13:23:32 +0530

    inreased payload size

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ dotenv.config();
 
 app.use(cors());
 
-app.use(express.json({ limit: '20mb' }));
-app.use(express.urlencoded({ limit: '20mb', extended: true }));
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ limit: '100mb', extended: true }));
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*'); // Allow requests from any origin
